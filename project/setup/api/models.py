9f00efb5289e7304bc9d57b8f32aa5604175a006
alstacon <alstacon@gmail.com>
2022-09-05 17:58:07 +0300

    added models User, Favorites

--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -20,5 +20,21 @@ movie: Model = api.model('Фильм', {
     'year': fields.Integer(required=True, example=2000),
     'rating': fields.Float(required=True, example=6.7),
     'genre_id': fields.Integer(required=True, example=1),
-    'director_id': fields.Integer(required=True, example=1)
+    'director_id': fields.Integer(required=True, example=1),
+    'genre': fields.Nested(genre),
+    'director' : fields.Nested(director)
+})
+
+user: Model = api.model('Пользователь', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, example="email"),
+    'password': fields.String(required=True, example="password"),
+    'name': fields.String(required=True, example='name'),
+    'surname': fields.String(required=True, example='surname'),
+    'favorite_genre': fields.Nested(genre)
+})
+
+favorites: Model = api.model('Избранное',{
+    'user_id': fields.Integer(required=True, example=1),
+    'movie_id': fields.Integer(required=True, example=1)
 })
