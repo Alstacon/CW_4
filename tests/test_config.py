f050e02765b1c1174d5c3bfd6e3763726210b5d3
alstacon <alstacon@gmail.com>
2023-02-20 11:01:43 +0400

    tests action

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+import os
+
 from project.config import BASE_DIR, DevelopmentConfig, TestingConfig
 from project.server import create_app
 
@@ -13,4 +15,10 @@ class TestConfig:
     def test_testing(self):
         app_config = create_app(TestingConfig).config
         assert app_config["TESTING"] is True
-        assert app_config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///test.db"
+        assert app_config["DATABASE_URI"] == "postgresql://{username}:{password}@{host}:{port}/{db_name}".format(
+        username=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        port=int(os.getenv('POSTGRES_PORT', 5432)),
+        db_name=os.getenv('POSTGRES_DB')
+    )
